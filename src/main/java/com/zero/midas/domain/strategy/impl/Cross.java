9e8e0ea0e07d243e4d766372998e669615869ca7
fengzijian <393390162@qq.com>
2020-08-18 10:38:43 +0800

    十字线

--- src/main/java/com/zero/midas/domain/strategy/impl/Cross.java
+++ src/main/java/com/zero/midas/domain/strategy/impl/Cross.java
@@ -5,6 +5,7 @@ import com.zero.midas.domain.entity.kline.KLineNode;
 import com.zero.midas.domain.strategy.Pattern;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Component
@@ -12,6 +13,10 @@ public class Cross implements Pattern {
 
     @Override
     public boolean judge(List<KLineNode> kLines) {
-        return true;
+        if (kLines.isEmpty()) {
+            return false;
+        }
+        KLineNode kLineNode = kLines.get(kLines.size() - 1);
+        return kLineNode.entityRatio().compareTo(new BigDecimal("0.01")) < 0;
     }
 }

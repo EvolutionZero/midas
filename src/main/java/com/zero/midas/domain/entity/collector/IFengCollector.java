793a9e146bb8db72124c85132bd7012d3eab68e1
fengzijian <393390162@qq.com>
2020-08-18 11:33:00 +0800

    使用spring管理多例

--- src/main/java/com/zero/midas/domain/entity/collector/IFengCollector.java
+++ src/main/java/com/zero/midas/domain/entity/collector/IFengCollector.java
@@ -3,18 +3,24 @@ package com.zero.midas.domain.entity.collector;
 import com.zero.midas.domain.factory.IFengCollectorFactory;
 import com.zero.midas.domain.model.dto.TradeDTO;
 import com.zero.midas.remote.facade.IFengFinanceFacade;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class IFengCollector {
     private String code;
+    @Autowired
     private IFengFinanceFacade client;
+    @Autowired
     private IFengCollectorFactory factory;
 
-    public IFengCollector(String code, IFengFinanceFacade client, IFengCollectorFactory factory) {
+    public IFengCollector(String code) {
         this.code = code;
-        this.client = client;
-        this.factory = factory;
     }
 
     public List<TradeDTO> daliyTrade() {
@@ -28,4 +34,5 @@ public class IFengCollector {
     public List<TradeDTO> monthlyTrade() {
         return this.factory.toTradeDTOList(this.code, this.client.queryMonthly(this.code));
     }
+
 }
--- src/main/java/com/zero/midas/domain/manager/CollectorManager.java
+++ src/main/java/com/zero/midas/domain/manager/CollectorManager.java
@@ -1,19 +1,17 @@
 package com.zero.midas.domain.manager;
 
 import com.zero.midas.domain.entity.collector.IFengCollector;
-import com.zero.midas.domain.factory.IFengCollectorFactory;
-import com.zero.midas.remote.facade.IFengFinanceFacade;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CollectorManager {
+
     @Autowired
-    private IFengCollectorFactory iFengCollectorFactory;
-    @Autowired
-    private IFengFinanceFacade iFengFinanceFacade;
+    private BeanFactory beanFactory;
 
     public IFengCollector createIFengCollector(String code) {
-        return new IFengCollector(code, iFengFinanceFacade, this.iFengCollectorFactory);
+        return beanFactory.getBean(IFengCollector.class, code);
     }
 }

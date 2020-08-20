810b7c6af93e0499c73125c986ba5a7407432255
fengzijian <393390162@qq.com>
2020-08-20 17:38:31 +0800

    采集周线月线去除本周期的数据

--- src/main/java/com/zero/midas/domain/entity/collector/IFengCollector.java
+++ src/main/java/com/zero/midas/domain/entity/collector/IFengCollector.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.WeekFields;
 import java.util.List;
 
 @Component
@@ -28,11 +31,28 @@ public class IFengCollector {
     }
 
     public List<TradeDTO> weeklyTrade() {
-        return this.factory.toTradeDTOList(this.code, this.client.queryWeekly(this.code));
+        List<TradeDTO> tradeDTOS = this.factory.toTradeDTOList(this.code, this.client.queryWeekly(this.code));
+        if (!tradeDTOS.isEmpty()) {
+            LocalDate lastTradeDate = tradeDTOS.get(tradeDTOS.size() - 1).getDate();
+            WeekFields weekFields = WeekFields.of(DayOfWeek.MONDAY, 1);
+            if (LocalDate.now().getYear() == lastTradeDate.getYear()
+                    && LocalDate.now().get(weekFields.weekOfYear()) == lastTradeDate.get(weekFields.weekOfYear())) {
+                tradeDTOS = tradeDTOS.subList(0, tradeDTOS.size() - 1);
+            }
+        }
+        return tradeDTOS;
     }
 
     public List<TradeDTO> monthlyTrade() {
-        return this.factory.toTradeDTOList(this.code, this.client.queryMonthly(this.code));
+        List<TradeDTO> tradeDTOS = this.factory.toTradeDTOList(this.code, this.client.queryMonthly(this.code));
+        if (!tradeDTOS.isEmpty()) {
+            LocalDate lastTradeDate = tradeDTOS.get(tradeDTOS.size() - 1).getDate();
+            if (LocalDate.now().getYear() == lastTradeDate.getYear()
+                    && LocalDate.now().getMonthValue() == lastTradeDate.getMonthValue()) {
+                tradeDTOS = tradeDTOS.subList(0, tradeDTOS.size() - 1);
+            }
+        }
+        return tradeDTOS;
     }
 
 }
